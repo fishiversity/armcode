519277884abff6058dfd11ff6eb780cdc16d5a2e
fishiversity <41026299+fishiversity@users.noreply.github.com>
2022-12-20 07:56:07 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    I = receivedNumber
+})
+let I = 0
+radio.setGroup(0)
+I = 99
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 basic.forever(function () {
-	
+    if (I == 1) {
+    	
+    }
 })
