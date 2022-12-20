7e6c0d0143107e6583633861d8146df71f9b4a0a
mifish3474 <121108427+mifish3474@users.noreply.github.com>
2022-12-20 09:20:28 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,23 @@
 radio.onReceivedNumber(function (receivedNumber) {
     I = receivedNumber
 })
+let V = 0
 let I = 0
 radio.setGroup(0)
 I = 99
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
 basic.forever(function () {
     if (I == 1) {
-    	
+        if (V < 90) {
+            V = V + 2
+            basic.pause(100)
+            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, V)
+        }
+    } else if (I == 2) {
+        if (V > 0) {
+            V = V - 2
+            basic.pause(100)
+            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, V)
+        }
     }
 })
